e7ed070e28ed9308d690e8456fb6b688fe04be42
cyuab <cyuab@connect.ust.hk>
2026-01-12 10:25:09 +0900

    Enhance grid search scripts for electricity and traffic datasets with additional parameters for motif information and normalization

--- python/run_grid_search.sh
+++ python/run_grid_search.sh
@@ -8,10 +8,43 @@ echo "Starting Grid Search: electricity, include_motif_information=0..."
 python grid_search_electricity.py \
     --include_covariates True False
 
+echo "Starting Grid Search: electricity, include_motif_information=1..."
+python grid_search_electricity.py \
+    --include_covariates True False \
+    --include_motif_information 1 \
+    --no_points_after_motif 1 2 6 12 24\
+    --do_normalization True False \
+    --include_similarity True False
+
+echo "Starting Grid Search: electricity, include_motif_information=2..."
+python grid_search_electricity.py \
+    --include_covariates True False \
+    --include_motif_information 2 \
+    --no_points_after_motif 1 2 6 12 24\
+    --do_normalization True False \
+    --include_similarity True False
+
+
 echo "For traffic..."
 echo "Starting Grid Search: traffic, include_motif_information=0..."
 python grid_search_traffic.py \
     --include_covariates True False
+
+echo "Starting Grid Search: traffic, include_motif_information=1..."
+python grid_search_traffic.py \
+    --include_covariates True False \
+    --include_motif_information 1 \
+    --no_points_after_motif 1 2 6 12 24\
+    --do_normalization True False \
+    --include_similarity True False
+
+echo "Starting Grid Search: traffic, include_motif_information=2..."
+python grid_search_traffic.py \
+    --include_covariates True False \
+    --include_motif_information 2 \
+    --no_points_after_motif 1 2 6 12 24\
+    --do_normalization True False \
+    --include_similarity True False
 
 # echo "For pemds7..."
 # echo "Starting Grid Search: pemds7, include_motif_information=0..."
@@ -22,7 +55,7 @@ python grid_search_traffic.py \
 # python grid_search_pemds7.py \
 #     --include_covariates True False \
 #     --include_motif_information 1 \
-#     --no_points_after_motif 1 5 9 \
+#     --no_points_after_motif 1 2 3 5 9 \
 #     --do_normalization True False \
 #     --include_similarity True False
 
@@ -30,13 +63,29 @@ python grid_search_traffic.py \
 # python grid_search_pemds7.py \
 #     --include_covariates True False \
 #     --include_motif_information 2 \
-#     --no_points_after_motif 1 5 9 \
+#     --no_points_after_motif 1 2 3 5 9 \
 #     --do_normalization True False \
 #     --include_similarity True False
 
-echo "For rate_exchange..."
-echo "Starting Grid Search: rate_exchange, include_motif_information=0..."
-python grid_search_rate_exchange.py \
-    --include_covariates True False
+# echo "For rate_exchange..."
+# echo "Starting Grid Search: rate_exchange, include_motif_information=0..."
+# python grid_search_rate_exchange.py \
+#     --include_covariates True False
+
+# echo "Starting Grid Search: rate_exchange, include_motif_information=1..."
+# python grid_search_rate_exchange.py \
+#     --include_covariates True False \
+#     --include_motif_information 1 \
+#     --no_points_after_motif 1 2 6 12 24\
+#     --do_normalization True False \
+#     --include_similarity True False
+
+# echo "Starting Grid Search: rate_exchange, include_motif_information=2..."
+# python grid_search_rate_exchange.py \
+#     --include_covariates True False \
+#     --include_motif_information 2 \
+#     --no_points_after_motif 1 2 6 12 24\
+#     --do_normalization True False \
+#     --include_similarity True False
 
 echo "All grid searches completed successfully."
