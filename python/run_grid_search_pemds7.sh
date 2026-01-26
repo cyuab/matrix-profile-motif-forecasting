ca6dfc44b7f660aa02cd78d63d0eb93ac95e7d60
cyuab <cyuab@connect.ust.hk>
2026-01-26 16:39:14 +0900

    test pemds7

--- python/run_grid_search_pemds7.sh
+++ python/run_grid_search_pemds7.sh
@@ -5,11 +5,11 @@ set -e
 
 # ================= Grid Search: pemds7 =================
 echo "For pemds7..."
-echo "Starting Grid Search: pemds7, include_motif_information=0..."
+echo "Starting Grid Search: pemds7, include_motif_information=0"
 python grid_search_pemds7.py \
     --include_covariates True False
 
-echo "Starting Grid Search: pemds7, include_motif_information=1..."
+echo "Starting Grid Search: pemds7, include_motif_information=1"
 python grid_search_pemds7.py \
     --include_covariates True False \
     --include_motif_information 1 \
@@ -17,7 +17,7 @@ python grid_search_pemds7.py \
     --do_normalization True False \
     --include_similarity True False
 
-echo "Starting Grid Search: pemds7, include_motif_information=2..."
+echo "Starting Grid Search: pemds7, include_motif_information=2"
 python grid_search_pemds7.py \
     --include_covariates True False \
     --include_motif_information 2 \
@@ -32,10 +32,27 @@ python grid_search_pemds7.py \
     --no_points_after_motif 9 \
     --include_similarity True False
 
-echo "Starting Grid Search: pemds7, include_motif_information=7, 9, 11..."
+echo "Starting Grid Search: pemds7, include_motif_information=7, 9, 11"
 python grid_search_pemds7.py \
     --include_covariates True \
     --include_motif_information 7 9 11 \
+    --no_points_after_motif 9 \
+    --include_similarity True False
+
+# k-NN
+echo "Starting Grid Search: pemds7, include_motif_information=13, 15, 17"
+python grid_search_pemds7.py \
+    --include_covariates True False \
+    --include_motif_information 13 15 17 \
+    --k_motifs 3 5 7 \
+    --no_points_after_motif 9 \
+    --include_similarity True False
+
+echo "Starting Grid Search: pemds7, include_motif_information=19, 21, 23"
+python grid_search_pemds7.py \
+    --include_covariates True \
+    --include_motif_information 19 21 23 \
+    --k_motifs 3 5 7 \
     --no_points_after_motif 9 \
     --include_similarity True False
 # ==========================================================
